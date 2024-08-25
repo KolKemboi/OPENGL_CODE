2c2f7460b7c31b64aa04adee3d2ff183f413e27e
Kemboi Cheruiyot <87800637+KolKemboi@users.noreply.github.com>
2024-08-25 22:59:24 +0300

    Update

--- Camera/shaderClass.cpp
+++ Camera/shaderClass.cpp
@@ -5,8 +5,11 @@ const char* vertSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
 "layout (location = 1) in vec3 aColor;\n"
 "out vec3 color;\n"
+"uniform mat4 model;\n"
+"uniform mat4 view;\n"
+"uniform mat4 proj;\n"
 "void main(){\n"
-"	gl_Position = vec4(aPos, 1.0);\n"
+"	gl_Position = proj * view * model * vec4(aPos, 1.0);\n"
 "	color = aColor;\n"
 "}\n\0";
 
