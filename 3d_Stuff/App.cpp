04703a8b06656272e25097ff1357b9e5b0c7fbf8
Kemboi Cheruiyot <87800637+KolKemboi@users.noreply.github.com>
2024-11-07 13:15:39 +0300

    update

--- 3d_Stuff/App.cpp
+++ 3d_Stuff/App.cpp
@@ -9,32 +9,88 @@
 
 #include "MeshLoader.h"
 #include "RenderSystem.h"
+#include "Camera.h"
 
 typedef unsigned int u_int;
 
 
 u_int width = 800, height = 800;
-
 float verts[] =
 {
-	// POSITION             //COLORS    
-	 0.5,   0.5,  0.0,    1.0,   1.0,   1.0, //WHITE
-	 0.5,  -0.5,  0.0,    1.0,   0.0,   0.0, //RED
 	-0.5,  -0.5,  0.0,    0.0,   1.0,   0.0, //GREEN
-	-0.5,   0.5,  0.0,    0.0,   0.0,   1.0, //BLUE
+	// POSITION           // COLORS
+	// Front face (Z = 0.5)
+	0.5,  0.5,  0.5,    1.0, 0.0, 0.0, // Top-right (red)
+	0.5, -0.5,  0.5,    0.0, 1.0, 0.0, // Bottom-right (green)
+   -0.5, -0.5,  0.5,    0.0, 0.0, 1.0, // Bottom-left (blue)
+   -0.5,  0.5,  0.5,    1.0, 1.0, 0.0, // Top-left (yellow)
+
+   // Back face (Z = -0.5)
+   0.5,  0.5, -0.5,    1.0, 0.0, 1.0, // Top-right (magenta)
+   0.5, -0.5, -0.5,    0.0, 1.0, 1.0, // Bottom-right (cyan)
+  -0.5, -0.5, -0.5,    1.0, 1.0, 1.0, // Bottom-left (white)
+  -0.5,  0.5, -0.5,    0.5, 0.5, 0.5, // Top-left (gray)
+
+  // Left face (X = -0.5)
+ -0.5,  0.5,  0.5,    1.0, 0.0, 0.0, // Top-front (red)
+ -0.5, -0.5,  0.5,    0.0, 1.0, 0.0, // Bottom-front (green)
+ -0.5, -0.5, -0.5,    0.0, 0.0, 1.0, // Bottom-back (blue)
+ -0.5,  0.5, -0.5,    1.0, 1.0, 0.0, // Top-back (yellow)
+
+ // Right face (X = 0.5)
+ 0.5,  0.5,  0.5,    1.0, 0.0, 1.0, // Top-front (magenta)
+ 0.5, -0.5,  0.5,    0.0, 1.0, 1.0, // Bottom-front (cyan)
+ 0.5, -0.5, -0.5,    1.0, 1.0, 1.0, // Bottom-back (white)
+ 0.5,  0.5, -0.5,    0.5, 0.5, 0.5, // Top-back (gray)
+
+ // Top face (Y = 0.5)
+ 0.5,  0.5,  0.5,    1.0, 0.5, 0.0, // Front-right (orange)
+-0.5,  0.5,  0.5,    0.5, 0.0, 1.0, // Front-left (purple)
+-0.5,  0.5, -0.5,    1.0, 1.0, 0.0, // Back-left (yellow)
+ 0.5,  0.5, -0.5,    0.0, 0.5, 0.5, // Back-right (teal)
+
+ // Bottom face (Y = -0.5)
+ 0.5, -0.5,  0.5,    0.0, 0.5, 1.0, // Front-right (light blue)
+-0.5, -0.5,  0.5,    0.5, 0.0, 0.5, // Front-left (violet)
+-0.5, -0.5, -0.5,    1.0, 0.5, 1.0, // Back-left (pink)
+ 0.5, -0.5, -0.5,    0.5, 1.0, 0.0, // Back-right (lime green)
 };
 
 u_int idxs[] =
 {
+	// Front face
 	0, 1, 2,
 	2, 3, 0,
+
+	// Back face
+	4, 5, 6,
+	6, 7, 4,
+
+	// Left face
+	8, 9, 10,
+	10, 11, 8,
+
+	// Right face
+	12, 13, 14,
+	14, 15, 12,
+
+	// Top face
+	16, 17, 18,
+	18, 19, 16,
+
+	// Bottom face
+	20, 21, 22,
+	22, 23, 20,
 };
+
 
 void closeWindow(GLFWwindow* window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) glfwSetWindowShouldClose(window, true);
 }
+
 RenderSystem renderer;
+Camera camera;
 
 int main()
 {
@@ -69,9 +125,20 @@ int main()
 	while (!(glfwWindowShouldClose(window)))
 	{
 		closeWindow(window);
+		/*
+		event callbacks, 
+			this allows me to edit the window width and height, 
+			then when it renders, 
+			it used the new width and height;
+		then render to a frame buffer;
+		then render to an ImGui window;
+		*/
+		myMesh.m_Transform.m_Scale = glm::vec3(1.0);//this is how I scale it up and down
+	
+
 		renderer.initScene();
 
-		renderer.Render(myMesh);
+		renderer.Render(myMesh, camera, width, height);
 
 
 		ImGui_ImplGlfw_NewFrame();
@@ -81,7 +148,6 @@ int main()
 		ImGui::Begin("Debug Window");
 		ImGui::Text("This Is A Debug Window");
 		ImGui::End();
-
 
 		ImGui::Render();
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
--- 3d_Stuff/RenderSystem.h
+++ 3d_Stuff/RenderSystem.h
@@ -5,7 +5,10 @@
 #include <GLFW/glfw3.h>
 #include "Shader.h"
 #include "MeshLoader.h"
-
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include "Camera.h"
 
 typedef unsigned int u_int;
 
@@ -15,11 +18,15 @@ public:
 	void initScene()
 	{
 		glClearColor(0.2, 0.1, 0.3, 1.0);
+		glEnable(GL_DEPTH_TEST);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	}
-	void Render(Mesh& mesh)
+	void Render(Mesh& mesh, Camera& camera, u_int width, u_int height)
 	{
 		mesh.m_Shader.useShader();
+		setModelMat(mesh);
+		camera.SetView(mesh);
+		camera.SetProjection(mesh, width, height);
 		glBindVertexArray(mesh.m_VertexArray);
 		glDrawElements(GL_TRIANGLES, mesh.m_indexCount, GL_UNSIGNED_INT, nullptr);
 		glBindVertexArray(0);
@@ -30,6 +37,23 @@ public:
 		glfwPollEvents();
 	}
 
+	void setModelMat(Mesh& mesh)
+	{
+		// Reset the model matrix
+		this->m_ModelMat = glm::mat4(1.0f);
+
+		// Apply transformations: Position, Rotation, and Scale
+		//this->m_ModelMat = glm::translate(this->m_ModelMat, mesh.m_Transform.m_Position);
+		//this->m_ModelMat = glm::rotate(this->m_ModelMat, glm::radians(mesh.m_Transform.m_Rotation.x), glm::vec3(1.0f, 0.0f, 0.0f)); // Rotate around X
+		//this->m_ModelMat = glm::rotate(this->m_ModelMat, glm::radians(mesh.m_Transform.m_Rotation.y), glm::vec3(0.0f, 1.0f, 0.0f)); // Rotate around Y
+		//this->m_ModelMat = glm::rotate(this->m_ModelMat, glm::radians(mesh.m_Transform.m_Rotation.z), glm::vec3(0.0f, 0.0f, 1.0f)); // Rotate around Z
+		this->m_ModelMat = glm::scale(this->m_ModelMat, mesh.m_Transform.m_Scale); // Apply scaling
+
+		// Set the model matrix uniform in the shader
+		glUniformMatrix4fv(glGetUniformLocation(mesh.m_Shader.retShader(), "model"), 1, GL_FALSE, glm::value_ptr(this->m_ModelMat));
+	}
+
 private:
 
+	glm::mat4 m_ModelMat = glm::mat4(1.0);
 };
