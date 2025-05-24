005d7bf01d582ccc1fca717e528db687e1638b06
KolKemboi <kolkemboi@gmail.com>
2025-05-24 18:06:40 +0300

    created a boiler plate

---
+++ boilerplate/run_app.sh
@@ -0,0 +1,17 @@
+
+#!/bin/bash
+
+#Exit on error
+set -e
+
+if [[ -d "build" ]]; then
+  echo "build folder exists, proceeding to build...\n"
+else
+  echo "build folder not found, making build folder...\n"
+  mkdir build
+fi
+
+cd build
+cmake ..
+make
+./OpenGLProject 
